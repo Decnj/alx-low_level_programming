0c43c4ed2a4edcabcf64f2c06d1affa9b79a8261
Decnj <nnamdi_njemanze@yahoo.com>
2023-07-17 06:24:10 -0700

    Run C files and betty

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -12,11 +12,10 @@ int main(void)
 	long long int d;
 	float f;
 
-printf("Size of a char: %zu byte(s)", sizeof(a));
-printf("Size of an int: %zu byte(s)", sizeof(b));
-printf("Size of a long int: %zu byte(s)", sizeof(c));
-printf("Size of a long long int: %zu byte(s)", sizeof(d));
-printf("Size of a float: %zu byte(s)", sizeof(f));
-
+printf("Size of a char: %lu byte(s)\n", sizeof(a));
+printf("Size of an int: %lu byte(s)\n", sizeof(b));
+printf("Size of a long int: %lu byte(s)\n", sizeof(c));
+printf("Size of a long long int: %lu byte(s)\n", sizeof(d));
+printf("Size of a float: %lu byte(s)\n", sizeof(f));
 return (0);
 }
